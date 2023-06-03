2db6573780322725e3eea469a1660e7767764189
Eric <e_chia0921@outlook.com>
2023-06-04 00:47:07 +0800

    done learning

--- src/index.js
+++ src/index.js
@@ -2,38 +2,44 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
+import { books } from './books'
+import Book from './Book'
 
 const BookList = () => {
   return (
-    <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-    </section>
+    <>
+      <h1>amazon best seller</h1>
+      <section className="booklist">
+        {/* <EventExamples /> */}
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />
+        })}
+      </section>
+    </>
   )
 }
 
-const Book = () => {
-  return (
-    <article className="book">
-      <Image />
-      <Title />
-      <Author />
-    </article>
-  )
-}
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/81uAjftkMtL._AC_UL900_SR900,600_.jpg"
-    alt="Why a Daughter Needs a Dad"
-  />
-)
-const Title = () => <h2>Why a Daughter Needs a Dad</h2>
-const Author = () => {
-  return <h4>Gregory E. Lang</h4>
-}
+// const EventExamples = () => {
+//   return (
+//     <section>
+//       <form>
+//         <h2>Typical Form</h2>
+//         <input
+//           type="text"
+//           name="product"
+//           onChange={(e) => console.log(e.target.value)}
+//           style={{ margin: '1rem 0' }}
+//         />
+//         <button type="submit">submit</button>
+//       </form>
+//       <div>
+//         <button onClick={() => console.log('click me')} type="button">
+//           click me
+//         </button>
+//       </div>
+//     </section>
+//   )
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<BookList />)
